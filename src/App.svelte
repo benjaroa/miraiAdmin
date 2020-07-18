8734b26fdda65adac919757bdc3eda89acad336f
Benja Roa <broa@creditu.com>
2020-07-17 21:45:26 -0400

    Modal copiado de svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,68 @@
 <script>
-	export let name;
+  import Modal from "./Modal.svelte";
+
+  let showModal = false;
+  export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  <button on:click={() => (showModal = true)}>show modal</button>
+</main>
+{#if showModal}
+  <Modal on:close={() => (showModal = false)}>
+    <h2 slot="header">
+      modal
+      <small>
+        <em>adjective</em>
+        mod·al \ˈmō-dəl\
+      </small>
+    </h2>
+
+    <ol class="definition-list">
+      <li>of or relating to modality in logic</li>
+      <li>
+        containing provisions as to the mode of procedure or the manner of
+        taking effect —used of a contract or legacy
+      </li>
+      <li>of or relating to a musical mode</li>
+      <li>of or relating to structure as opposed to substance</li>
+      <li>
+        of, relating to, or constituting a grammatical form or category
+        characteristically indicating predication
+      </li>
+      <li>of or relating to a statistical mode</li>
+    </ol>
+
+    <a href="https://www.merriam-webster.com/dictionary/modal">
+      merriam-webster.com
+    </a>
+  </Modal>
+{/if}
